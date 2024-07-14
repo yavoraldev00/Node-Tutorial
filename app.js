adb535c1eec9364fdc656cd429017124af2bd94c
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-14 22:42:39 +0200

    tutorial 7 - view engines / changed index ejs script

--- app.js
+++ app.js
@@ -3,21 +3,28 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(3000);
 
 app.get("/",(req,res) => {
-    res.sendFile("./views/index.html", { root: __dirname });
+    res.render("index", {title: "Home", blogs: [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ]})
 })
 
 app.get("/about",(req,res) => {
-    res.sendFile("./views/about.html", { root: __dirname });
+    res.render("about",{title: "About"})
 })
 
-app.get("/about-me",(req,res) => {
-    res.redirect("/about");
+app.get("/blogs/create",(req,res) => {
+    res.render("create", {title: "Create Blog"});
 })
 
 app.use((req,res) => {
-    res.status(404).sendFile("./views/404.html", { root: __dirname })
+    res.status(404).render("404", {title: "404"})
 })
